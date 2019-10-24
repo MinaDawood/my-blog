b99649cc1c9afa31205097e3cb735bc61ff3887b
Mina Amir Sammy <heliopolis.development@gmail.com>
2019-10-24 16:14:14 +0200

    First Commit

---
+++ src/build/precache-manifest.0a92abd10d1e5bf60ffdd3937ec06903.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5da138fea397cae8435dcc03e1c48ea7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c1ac7acf56aa99bc5ff3",
+    "url": "/static/css/main.73ff5b42.chunk.css"
+  },
+  {
+    "revision": "4f6c96cb21b962f683d0",
+    "url": "/static/js/2.7e5581d6.chunk.js"
+  },
+  {
+    "revision": "c1ac7acf56aa99bc5ff3",
+    "url": "/static/js/main.724111e4.chunk.js"
+  },
+  {
+    "revision": "5d3df65c6aad5af6f7eb",
+    "url": "/static/js/runtime-main.8a83d180.js"
+  }
+]);
